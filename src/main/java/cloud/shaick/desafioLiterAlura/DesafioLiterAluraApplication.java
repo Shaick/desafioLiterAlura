a53ca46ccfc529e8e2c0004296f0dd2650b7e5a5
Wilson Cardoso (shaick) <shaick@msn.com>
2024-07-02 01:26:20 -0300

    ok2

--- src/main/java/cloud/shaick/desafioLiterAlura/DesafioLiterAluraApplication.java
+++ src/main/java/cloud/shaick/desafioLiterAlura/DesafioLiterAluraApplication.java
@@ -1,13 +1,19 @@
 package cloud.shaick.desafioLiterAlura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioLiterAluraApplication {
+public class DesafioLiterAluraApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioLiterAluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("");
 	}
 
 }
