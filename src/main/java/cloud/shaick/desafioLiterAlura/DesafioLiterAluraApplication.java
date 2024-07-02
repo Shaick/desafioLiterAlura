20805413c9bdbce2816c7984cdf238e3f12feb45
Wilson Cardoso (shaick) <shaick@msn.com>
2024-07-02 00:34:20 -0300

    projeto inicial

---
+++ src/main/java/cloud/shaick/desafioLiterAlura/DesafioLiterAluraApplication.java
@@ -0,0 +1,13 @@
+package cloud.shaick.desafioLiterAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLiterAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioLiterAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cloud/shaick/desafioLiterAlura/DesafioLiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.shaick.desafioLiterAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
